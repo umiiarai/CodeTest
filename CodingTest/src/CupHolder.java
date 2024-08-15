36f339ee462fe20492a438fefd8c90a0d009841e
flower-ocean <thddbap@gmail.com>
2024-08-16 08:07:17 +0900

    [Baekjoon] No.2810 컵홀더 - 구현 - 문자열 - 그리디 알고리즘

---
+++ CodingTest/src/CupHolder.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CupHolder {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		String s = br.readLine();
+		String str = "";
+		
+		for (int i=0; i<n; i++) {
+			if (s.charAt(i) == 'S') { // S(일반석이면) * 추가
+				str += "*S";
+			} else {
+				str += "*LL"; // L(커플석이면) 2칸 넘어간 후 * 추가
+				i++;
+			}
+		}
+		
+		str += "*"; // 마지막 자리에 * 추가
+		int count = 0;
+		for (int i=0; i<str.length(); i++) {
+			if (str.charAt(i) == '*') count++;
+		}
+		count = Math.min(count, n);
+		System.out.println(count);
+	}
+
+}
